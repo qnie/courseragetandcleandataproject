73e375ec511039035ca5fb82fae3fda0d574c563
qnie <qnie0207@gmail.com>
2014-07-20 23:03:56 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,47 +2,60 @@ courseragetandcleandataproject
 ==============================
 ## Change directory to "D:/UCI HAR Dataset"
 
-## Step 1
-setwd("train")
- Xtrain<-read.table("X_train.txt")
- Ytrain<-read.table("Y_train.txt")
- strain<-read.table("subject_train.txt")
-
- setwd("test")
- Xtest<-read.table("X_test.txt")
- Ytest<-read.table("Y_test.txt")
- stext<-read.table("subject_test.txt")
- stest<-read.table("subject_test.txt")
-
-## Merge
- merge1<-rbind(Xtrain, Xtest)
- merge2<-rbind(strain, stest)
- merge3<-rbind(Ytrain, Ytest)
-
- data1<-cbind(merge1, merge2, merge3)
+# Read in the testing and training data
+subject_test  <- read.delim(file="D:/UCI HAR Dataset/test/subject_test.txt", col.names="subject")
+subject_train <- read.delim(file="D:/UCI HAR Dataset/train/subject_train.txt", col.names="subject")
+y_test        <- read.delim(file="D:/UCI HAR Dataset/test/y_test.txt", col.names="activity")
+y_train       <- read.delim(file="D:/UCI HAR Dataset/train/y_train.txt", col.names="activity")
 
 
+# Get the number of rows in the test / training sets
+test_rows <- nrow(y_test)
+train_rows <- nrow(y_train)
 
-## Step 2
-##read features.txt file
-step2 = read.table("features.txt", col.names=c("step2_id", "step2_labels"),)
-selec_step2 <- step2[grepl("mean\\(\\)", step2$step2_labels) | grepl("std\\(\\)", step2$step2_labels), ]
-data2 <- merge[, c(c(1, 2, 3), selec_step2$step2_id + 3) ]
+# Scan in the feature variables
+raw_X_test    <- scan(file="C:/Users/Administrator/UCI HAR Dataset/test/X_test.txt", nlines=test_rows)
+raw_X_train   <- scan(file="C:/Users/Administrator/UCI HAR Dataset/train/X_train.txt", nlines=train_rows)
 
+# Read in the column names for each of the features in X
+feature_names <- read.table("C:/Users/Administrator/UCI HAR Dataset/features.txt", sep=" ", col.names=c("column","name"))
 
-## step 3
-## read file 
-step3 = read.table("activity_labels.txt", col.names=c("activity_id", "activity_labels"),) 
-data3 <- merge(data1, activity_labels)
+# Read in the activity labels
+activity_labels <- read.table("C:/Users/Administrator/UCI HAR Dataset/activity_labels.txt", sep=" ", col.names=c("level","label"))
 
+# Get the number of features
+nFeatures <- nrow(feature_names)
 
+# Create a dataframe from the raw X features data
+X_test <- as.data.frame(matrix(raw_X_test, nrow=test_rows, ncol=nFeatures, byrow=TRUE))
+X_train <- as.data.frame(matrix(raw_X_train, nrow=train_rows, ncol=nFeatures, byrow=TRUE))
 
-## step 4
-library(reshape)
-library(reshape2)
-step4<-rename(selec_step2, c(step2_id="Subject",step2_labels="Activity"))
+# Append the training data to the testing data
+subject <- rbind(subject_test, subject_train)
+y       <- rbind(y_test, y_train)
+X_raw   <- rbind(X_test, X_train)
 
- 
-## step 5
-step5 <- dcast(step4 , Subject + Activity ~ variable, mean)
-write.table(step5, file="TinyData.txt", sep = " ")
+# Get the mean and std dev features columns
+mean_std_cols <- sort(c(grep("mean\\(\\)",feature_names$name), grep("std\\(\\)",feature_names$name)))
+features <- feature_names[mean_std_cols, ]
+
+# Subset the data to only include the mean and std dev features
+X <- X_raw[, mean_std_cols] 
+names(X) <- features$name
+
+# Combine the datasets
+final_data <- cbind(subject, y, X)
+
+# Change the activities to factors and set the proper labels
+final_data$activity <- as.factor(final_data$activity)
+levels(final_data$activity) <- activity_labels$label
+
+# Create a tidy dataset with the average of each feature for each activity
+tidy_data <- aggregate(final_data[,3:ncol(final_data)]
+                       , list(final_data$activity, final_data$subject)
+                       , mean)
+names(tidy_data)[1] <- "activity"
+names(tidy_data)[2] <- "subject"
+
+# Save the tidy data set in the data folder as "tidy_data.txt"
+write.table(tidy_data, "C:/Users/Administrator/tidy_data.txt")
